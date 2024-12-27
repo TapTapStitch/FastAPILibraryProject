b29b55d131ec1c5400429d64c313bd0ee0f99185
Mykyta Zub <Mykyta_Zub@epam.com>
2024-12-27 15:43:23 +0200

    fix

--- app/models/book.py
+++ app/models/book.py
@@ -3,7 +3,7 @@ from ..config import Base
 
 
 class Book(Base):
-    __tablename__ = "book"
+    __tablename__ = "books"
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
