345a39ac37ffcb718ac80ddceba06f6b342958fa
Mykyta Zub <Mykyta_Zub@epam.com>
2024-12-31 10:35:51 +0200

    black

--- app/schemas/book.py
+++ app/schemas/book.py
@@ -7,7 +7,7 @@ class BookSchema(BaseModel):
     title: str
     description: str
     year_of_publication: int = Field(..., ge=1000, le=9999)
-    isbn: str = Field(..., min_length=13, max_length=13, pattern=r'^\d{13}$')
+    isbn: str = Field(..., min_length=13, max_length=13, pattern=r"^\d{13}$")
     created_at: datetime
     updated_at: datetime
 
@@ -16,7 +16,7 @@ class ChangeBookSchema(BaseModel):
     title: str
     description: str | None = ""
     year_of_publication: int = Field(..., ge=1000, le=9999)
-    isbn: str = Field(..., min_length=13, max_length=13, pattern=r'^\d{13}$')
+    isbn: str = Field(..., min_length=13, max_length=13, pattern=r"^\d{13}$")
 
     class Config:
         extra = "forbid"
