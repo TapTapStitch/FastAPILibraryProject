8ab7ba49cd673c0e5850551d523d3a4de4a5a782
Mykyta Zub <Mykyta_Zub@epam.com>
2025-01-03 21:20:17 +0200

    added proper way of managing book_author association

--- app/crud/authors.py
+++ app/crud/authors.py
@@ -1,24 +1,24 @@
-from sqlalchemy.orm import Session, selectinload
+from sqlalchemy.orm import Session
 from sqlalchemy import desc, select
 from fastapi import HTTPException
 from ..services.pagination import paginate
+from ..models.book import Book
 from ..models.author import Author
+from ..models.book_author import BookAuthor
 from ..schemas.author import CreateAuthorSchema, UpdateAuthorSchema
+from ..schemas.pagination import PaginationParams
 
 
 class AuthorsCrud:
     def __init__(self, db: Session):
         self.db = db
 
-    def get_authors(self, pagination):
+    def get_authors(self, pagination: PaginationParams):
         stmt = select(Author).order_by(desc(Author.created_at))
         return paginate(self.db, stmt=stmt, pagination=pagination)
 
     def get_author_by_id(self, author_id: int):
-        author = self._get_author_by_id(author_id)
-        if not author:
-            raise HTTPException(status_code=404, detail="Author not found")
-        return author
+        return self._get_author_by_id(author_id)
 
     def create_author(self, author_data: CreateAuthorSchema):
         author = Author(
@@ -33,7 +33,7 @@ class AuthorsCrud:
         return author
 
     def update_author(self, author_id: int, author_data: UpdateAuthorSchema):
-        author = self.get_author_by_id(author_id=author_id)
+        author = self._get_author_by_id(author_id=author_id)
         updated_data = author_data.model_dump(exclude_unset=True)
 
         for field, value in updated_data.items():
@@ -44,11 +44,66 @@ class AuthorsCrud:
         return author
 
     def remove_author(self, author_id: int):
-        author = self.get_author_by_id(author_id=author_id)
+        author = self._get_author_by_id(author_id=author_id)
         self.db.delete(author)
         self.db.commit()
 
+    def get_books_of_author(self, author_id: int, pagination: PaginationParams):
+        self._get_author_by_id(author_id)
+
+        stmt = (
+            select(Book)
+            .join(BookAuthor, Book.id == BookAuthor.book_id)
+            .where(BookAuthor.author_id == author_id)
+        )
+        return paginate(self.db, stmt=stmt, pagination=pagination)
+
+    def create_author_book_association(self, author_id: int, book_id: int):
+        self._get_author_by_id(author_id)
+
+        self._check_book_exists(book_id)
+
+        association_exists = self.db.execute(
+            select(BookAuthor)
+            .where(BookAuthor.book_id == book_id)
+            .where(BookAuthor.author_id == author_id)
+        ).scalar_one_or_none()
+        if association_exists:
+            raise HTTPException(
+                status_code=400,
+                detail="Association already exists",
+            )
+
+        book_author = BookAuthor(book_id=book_id, author_id=author_id)
+        self.db.add(book_author)
+        self.db.commit()
+
+    def remove_author_book_association(self, author_id: int, book_id: int):
+        self._get_author_by_id(author_id)
+
+        self._check_book_exists(book_id)
+
+        association = self.db.execute(
+            select(BookAuthor)
+            .where(BookAuthor.book_id == book_id)
+            .where(BookAuthor.author_id == author_id)
+        ).scalar_one_or_none()
+        if not association:
+            raise HTTPException(status_code=404, detail="Association not found")
+
+        self.db.delete(association)
+        self.db.commit()
+
     def _get_author_by_id(self, author_id: int):
-        return self.db.execute(
+        author = self.db.execute(
             select(Author).where(Author.id == author_id)
         ).scalar_one_or_none()
+        if not author:
+            raise HTTPException(status_code=404, detail="Author not found")
+        return author
+
+    def _check_book_exists(self, book_id: int):
+        if not self.db.execute(
+            select(Book).where(Book.id == book_id)
+        ).scalar_one_or_none():
+            raise HTTPException(status_code=404, detail="Book not found")
--- app/routers/authors.py
+++ app/routers/authors.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response
 from ..config import get_db
 from sqlalchemy.orm import Session
 from ..schemas.author import AuthorSchema, CreateAuthorSchema, UpdateAuthorSchema
+from ..schemas.book import BookSchema
 from ..schemas.pagination import PaginationParams, PaginatedResponse
 from ..crud.authors import AuthorsCrud
 
@@ -101,3 +102,107 @@ async def update_author(
 )
 async def delete_author(author_id: int, crud: AuthorsCrud = Depends(get_authors_crud)):
     return crud.remove_author(author_id=author_id)
+
+
+@router.get(
+    "/{author_id}/books",
+    response_model=PaginatedResponse[BookSchema],
+    responses={
+        404: {
+            "description": "Not Found",
+            "content": {
+                "application/json": {
+                    "examples": {
+                        "author_not_found": {
+                            "summary": "Author not found",
+                            "value": {"detail": "Author not found"},
+                        }
+                    }
+                }
+            },
+        },
+    },
+)
+def get_books_of_author(
+    author_id: int,
+    pagination: PaginationParams = Depends(),
+    crud: AuthorsCrud = Depends(get_authors_crud),
+):
+    return crud.get_books_of_author(author_id=author_id, pagination=pagination)
+
+
+@router.post(
+    "/{author_id}/books/{book_id}",
+    status_code=201,
+    responses={
+        404: {
+            "description": "Not Found",
+            "content": {
+                "application/json": {
+                    "examples": {
+                        "author_not_found": {
+                            "summary": "Author not found",
+                            "value": {"detail": "Author not found"},
+                        },
+                        "book_not_found": {
+                            "summary": "Book not found",
+                            "value": {"detail": "Book not found"},
+                        },
+                    }
+                }
+            },
+        },
+        400: {
+            "description": "Bad Request",
+            "content": {
+                "application/json": {
+                    "examples": {
+                        "association_exists": {
+                            "summary": "Association exists",
+                            "value": {"detail": "Association already exists"},
+                        }
+                    }
+                }
+            },
+        },
+    },
+)
+def create_author_book_association(
+    author_id: int, book_id: int, crud: AuthorsCrud = Depends(get_authors_crud)
+):
+    crud.create_author_book_association(author_id=author_id, book_id=book_id)
+    return Response(status_code=201)
+
+
+@router.delete(
+    "/{author_id}/books/{book_id}",
+    status_code=204,
+    responses={
+        404: {
+            "description": "Not Found",
+            "content": {
+                "application/json": {
+                    "examples": {
+                        "author_not_found": {
+                            "summary": "Author not found",
+                            "value": {"detail": "Author not found"},
+                        },
+                        "book_not_found": {
+                            "summary": "Book not found",
+                            "value": {"detail": "Book not found"},
+                        },
+                        "association_not_found": {
+                            "summary": "Association not found",
+                            "value": {"detail": "Association not found"},
+                        },
+                    }
+                }
+            },
+        },
+    },
+)
+async def delete_book_author_association(
+    book_id: int, author_id: int, crud: AuthorsCrud = Depends(get_authors_crud)
+):
+    crud.remove_author_book_association(author_id=author_id, book_id=book_id)
+    return Response(status_code=204)
