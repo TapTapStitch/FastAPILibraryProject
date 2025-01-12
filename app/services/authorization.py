f7104a16fd01eb9a668d5e7635426009b6a76d10
Mykyta Zub <Mykyta_Zub@epam.com>
2025-01-12 13:02:01 +0200

    using HTTPBearer

--- app/services/authorization.py
+++ app/services/authorization.py
@@ -1,12 +1,12 @@
 import jwt
 from datetime import datetime, timedelta, timezone
 from fastapi import HTTPException, Depends
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPBearer
 from app.config import settings
 from ..schemas.token import Token
 
 
-oauth2_schema = OAuth2PasswordBearer(tokenUrl="sessions/sign_in")
+auth_bearer = HTTPBearer()
 
 
 def create_jwt_token(user_id: int):
@@ -16,7 +16,7 @@ def create_jwt_token(user_id: int):
         + timedelta(minutes=settings.JWT_TOKEN_EXPIRATION),
     }
     token = jwt.encode(payload, settings.JWT_SECRET_KEY, algorithm="HS256")
-    return Token(access_token=token, token_type="bearer")
+    return Token(access_token=token, token_type="Bearer")
 
 
 def decode_jwt_token(token: str):
@@ -29,5 +29,6 @@ def decode_jwt_token(token: str):
         raise HTTPException(status_code=401, detail="Invalid token")
 
 
-def get_current_user_id(token: str = Depends(oauth2_schema)):
+def get_current_user_id(auth: str = Depends(auth_bearer)):
+    token = auth.credentials
     return int(decode_jwt_token(token)["sub"])
