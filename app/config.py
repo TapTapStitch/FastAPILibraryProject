45172523b95f86838237c3498904e1dfb5126e6c
Mykyta Zub <Mykyta_Zub@epam.com>
2025-02-05 11:20:46 +0200

    feat: add DEBUG configuration to environment and update engine initialization

--- app/config.py
+++ app/config.py
@@ -8,12 +8,13 @@ class Settings(BaseSettings):
     JWT_SECRET_KEY: str
     JWT_TOKEN_EXPIRATION: int | None = 60
     TEST_DATABASE_URL: str | None = None
+    DEBUG: bool | None = False
 
     model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
-engine = create_engine(settings.DATABASE_URL, echo=True)
+engine = create_engine(settings.DATABASE_URL, echo=settings.DEBUG)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
